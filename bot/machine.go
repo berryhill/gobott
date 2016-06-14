03d271d21aec3384d84f7bebf073a7931b7ed3e3
MachineShop <machineshop@MachineShops-MacBook-Pro.local>
2016-06-14 17:08:15 -0600

    implemented: AnalogSensor Unmarshal(), AnalogSensor Save()

--- bot/machine.go
+++ bot/machine.go
@@ -8,11 +8,15 @@ var MACHINE *models.Machine
 
 func init() {
 	MACHINE = models.NewMachine("Test Machine")
+
 	var sensor models.Sensor
 
 	sensor = models.NewAnalogSensor("PH Sensor")
 	MACHINE.AddSensor(&sensor)
 
 	sensor = models.NewAnalogSensor("PPM Sensor")
+	MACHINE.AddSensor(&sensor)
+
+	sensor = models.NewBooleanSensor("Button")
 	MACHINE.AddSensor(&sensor)
 }

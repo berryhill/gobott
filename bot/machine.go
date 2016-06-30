1eb7602c22e3534b0c44f63f7037241880b1c5d5
Berryhill <matthewberryhill@gmail.com>
2016-06-30 16:05:49 -0600

    Implmenting Thermistor 1

--- bot/machine.go
+++ bot/machine.go
@@ -15,7 +15,9 @@ var MACHINE *models.Machine
 var HeartBeatt *gpio.LedDriver
 var ReportIndicator *gpio.LedDriver
 var LightSensor *gpio.AnalogSensorDriver
-var LsValue interface{}
+var Thermistor *gpio.AnalogSensorDriver
+var LightSensorValue interface{}
+var ThermistorValue interface{}
 var mwork func()
 
 func init() {
@@ -24,11 +26,19 @@ func init() {
 	mwork = func() {
 		gobot.Every(1 * time.Second, func() {
 			Beat()
-			fmt.Println("Sensor: ", LsValue)
+			fmt.Println("Sensor: ", LightSensorValue)
+			fmt.Println("Sensor: ", ThermistorValue)
 		})
 
 		gobot.On(LightSensor.Event("data"), func(data interface{}) {
-			LsValue = data
+			LightSensorValue = data
+			//LsValue = uint8(
+			//	gobot.ToScale(gobot.FromScale(float64(data.(int)), 0, 1024), 0, 255),
+			//)
+			//fmt.Println("sensor", data)
+		})
+		gobot.On(Thermistor.Event("data"), func(data interface{}) {
+			ThermistorValue = data
 			//LsValue = uint8(
 			//	gobot.ToScale(gobot.FromScale(float64(data.(int)), 0, 1024), 0, 255),
 			//)
@@ -41,12 +51,14 @@ func NewMachineBot(b *beaglebone.BeagleboneAdaptor) *gobot.Robot {
  	HeartBeatt = gpio.NewLedDriver(b, "led", "P9_12")
 	ReportIndicator = gpio.NewLedDriver(b, "led", "P9_14")
 	LightSensor = gpio.NewAnalogSensorDriver(b, "LightSensor", "P9_33")
+	Thermistor = gpio.NewAnalogSensorDriver(b, "LightSensor", "P9_35")
 
 	robot := gobot.NewRobot("Peripheral Bot", []gobot.Connection{b},
 		[]gobot.Device {
 			HeartBeatt,
 			ReportIndicator,
 			LightSensor,
+			Thermistor,
 		}, mwork,
 	)
 
